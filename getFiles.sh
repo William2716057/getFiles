12aaa1f4bcefffa950391f8d4d163ffe5d63e490
User <Willspri@gmail.com>
2024-12-26 11:02:29 +1300

    changed to list

--- getFiles.sh
+++ getFiles.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 
-#URL
-URL="http://<IP>/path/to/file/example.txt
+#IP
+IP="<IP> #edit here
+
+
+#list of URLs
+URLS=(
+	"/path/to/file/example1.txt"
+	"/path/to/file/example2.txt"
+	"/path/to/file/example3.txt"
+)
+
+for URL in "${URLS[@]}"; do
+    echo "Downloading $URL"
+    wget "$URL"
+
+    # Check if wget was successful
+    if [ $? -eq 0 ]; then
+        echo "$URL download successful"
+    else
+        echo "$URL download failed"
+    fi
+done
 
 #command
 wget "$URL"
